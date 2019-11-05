39a6fa491db1c368a2e18041f9e19c1974c9fea8
saif ullah <saif559161@gmail.com>
2019-11-05 08:11:27 +0600

    state

--- app/App.js
+++ app/App.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
-import { Text, View, Platform } from 'react-native';
+import { Text, View, TouchableOpacity, Platform } from 'react-native';
 import Person from './Person';
 
 
-export default class App extends Component {
-  render() {
+export default class App extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+      name: "Saif Ullah",
+      email: "saif@gmail.com"
+    }
+  }
 
-    return (
-      <View style={{ marginTop:20 }}>
-        <Text>Person list goes here:</Text>
-        <Person name="saif" phone="01400" email="saif@gmail.com"/>
+  change(name, email){
+    this.setState({
+      name: name,
+      email: email
+    })
+
+    console.log(this.state);
+  }
+
+  
+  
+  
+
+  render(){
+    return(
+      <View style={{marginTop: 20}}>
+        <Text>Name: {this.state.name}</Text>
+        <Text>Email: {this.state.email}</Text>
+
+        <TouchableOpacity onPress={() => this.change('Mukta','mukta@gmail.com')}>
+          <Text>Change</Text>
+        </TouchableOpacity>
       </View>
-    );
+    )
   }
 }
