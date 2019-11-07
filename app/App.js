14aa662bf0ce86a6b13d823afdbede1e04470157
saif ullah <saif559161@gmail.com>
2019-11-07 11:01:22 +0600

    handling text input

--- app/App.js
+++ app/App.js
@@ -1,66 +1,28 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import Person from './Person';
 
 
-const styles = StyleSheet.create({
-  bigBlue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30
-  },
+export default class App extends Component{
 
-  red: {
-    color: 'red'
-  },
-
-  body:{
-    height: 300,
-    width: 300,
-    backgroundColor: 'green'
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
   }
 
-});
-
-
-export default class App extends Component{
   render(){
-    return(
-      // Try setting `flexDirection` to `column`.
-      // <View style={{flex: 1, flexDirection: 'row'}}>
-      //   <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-      //   <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-      //   <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
-      // </View>
-
-      // Try setting `justifyContent` to `center`.
-      // Try setting `flexDirection` to `row`.
-      // <View style={{
-      //   flex: 1,
-      //   flexDirection: 'row',
-      //   justifyContent: 'space-evenly',
-      // }}>
-      //   <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-      //   <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-      //   <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
-      // </View>
-
-      // Try setting `alignItems` to 'flex-start'
-      // Try setting `justifyContent` to `flex-end`.
-      // Try setting `flexDirection` to `row`.
-      <View style={{
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'flex-start',
-        alignSelf: 'flex-start',
-      }}>
-        <View style={{height: 100, backgroundColor: 'powderblue'}} />
-        <View style={{height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{height: 100, backgroundColor: 'steelblue'}} />
+    return (
+      <View style={{padding: 10}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '*').join(' ')}
+        </Text>
       </View>
-
-
-
-    )
+    );
   }
 }
